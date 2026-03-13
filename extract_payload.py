3505b4dd4d73c3b91f797d0a8e4c0fb3d5049078
NANDANA CM <nandanacm321@gmail.com>
2026-03-13 23:52:49 +0530

    Update extract_payload.py

--- extract_payload.py
+++ extract_payload.py
@@ -6,7 +6,8 @@ import hashlib
 def bits_to_text(bits):
     chars = []
     for i in range(0, len(bits), 8):
-        chars.append(chr(int(bits[i:i+8], 2)))
+        byte = bits[i:i+8]
+        chars.append(chr(int(byte, 2)))
     return ''.join(chars)
 
 
@@ -25,55 +26,69 @@ def extract_payload(audio_file, key, output_img):
 
     samples = np.frombuffer(frames, dtype=np.int16)
 
-    bits = ''.join([str(sample & 1) for sample in samples])
+    extracted_bits = []
+
+    def hash_func(msb, key):
+        return int(hashlib.sha256((str(msb) + key).encode()).hexdigest(), 16)
+
+    # read bits from correct positions
+    for sample in samples:
+
+        msb = (sample >> 8) & 0xFF
+
+        if hash_func(msb, key) % 10 == 0:
+
+            bit1 = (sample >> 1) & 1
+            bit2 = (sample >> 3) & 1
+
+            extracted_bits.append(str(bit1))
+            extracted_bits.append(str(bit2))
+
+    bits = ''.join(extracted_bits)
 
     pointer = 0
 
-    # HASH
-    hash_len = int(bits[pointer:pointer+32], 2)
+    # READ HEADER
+    mode = int(bits[pointer:pointer+8], 2)
+    pointer += 8
+
+    img_len = int(bits[pointer:pointer+32], 2)
     pointer += 32
 
-    hash_bits = bits[pointer:pointer+hash_len]
-    pointer += hash_len
-
-    received_hash = bits_to_text(hash_bits)
-
-    # TEXT
     text_len = int(bits[pointer:pointer+32], 2)
     pointer += 32
 
     message = ""
-
-    if text_len > 0:
-
-        text_bits = bits[pointer:pointer+text_len]
-        pointer += text_len
-
-        hidden_text = bits_to_text(text_bits)
-
-        verify_hash = hashlib.sha256(
-            (hidden_text + key).encode()
-        ).hexdigest()
-
-        if verify_hash == received_hash:
-            message = hidden_text
-        else:
-            message = "Hash verification failed"
+    image_found = False
 
     # IMAGE
-    img_len = int(bits[pointer:pointer+32], 2)
-    pointer += 32
-
-    mode = 0
-
     if img_len > 0:
 
         img_bits = bits[pointer:pointer+img_len]
+        pointer += img_len
+
         img_bytes = bits_to_bytes(img_bits)
 
         with open(output_img, "wb") as f:
             f.write(img_bytes)
 
-        mode = 1
+        image_found = True
 
-    return mode, message
+    # TEXT
+    if text_len > 0:
+
+        text_bits = bits[pointer:pointer+text_len]
+        pointer += text_len
+
+        message = bits_to_text(text_bits)
+
+    if image_found and message:
+        return 3, message
+
+    if image_found:
+        return 1, ""
+
+    if message:
+        return 2, message
+
+    return 0, ""
